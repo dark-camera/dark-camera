bc9c5fed8d9981abb1b0a173d079eea6884c6d62
Rishabh <rishabh@localhost.localdomain>
2018-11-21 11:05:42 +0530

    Uploaded

---
+++ git.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main(){
+  system("git add .");
+  system("git commit -m \"Uploaded\"");
+  system("git push -f origin master");
+  printf("RishabhRD\n");
+  printf("GithubLogin625");
+}
