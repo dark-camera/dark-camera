73e377d3224001f04aa5d36eb9217f06bd3cc386
Anuroop Singh <39482727+anuroop18@users.noreply.github.com>
2018-11-21 11:51:04 +0530

    Update auto_capture.py

--- auto_capture.py
+++ auto_capture.py
@@ -6,14 +6,14 @@ import os
 path = str(Path.home())
 i=0
 try:
-    os.mkdir(path+'/.image')
+    os.mkdir(path+'/watcher/')
 except: pass
 
 while True:
     i+=1
     cap = cv2.VideoCapture(0)
     ret, frame = cap.read()
-    name = path + '/watcher/img' + str(i) + '.png'
+    name = path + 'img' + str(i) + '.png'
     cv2.imwrite(name,frame)
     cap.release()
     cv2.destroyAllWindows()
