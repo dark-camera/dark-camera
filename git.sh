6d8dab4d4c525b33e036d7ff62918c478b809dc5
Rishabh <rishabh@localhost.localdomain>
2018-11-21 11:21:33 +0530

    Message

--- git.sh
+++ git.sh
@@ -1,3 +1,3 @@
 git add . ;
 git commit -m "Message";
-git push https://RishabhRD:GithubLogin625@dark-camera/dark-camera.git --all
+git push https://RishabhRD:GithubLogin625@github.com/dark-master/dark-master.git --all
